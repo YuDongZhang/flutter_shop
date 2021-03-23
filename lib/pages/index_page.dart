dccaed835936d734952e14d9e2949e35dcdf72ab
YuDongZhang <977872239@qq.com>
2021-03-23 16:27:46 +0800

    04.底部导航栏切换效果

--- lib/pages/index_page.dart
+++ lib/pages/index_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';//这个是苹果的风格
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_shop/pages/cart_page.dart';
+import 'package:flutter_shop/pages/category_page.dart';
+import 'package:flutter_shop/pages/home_page.dart';
+import 'package:flutter_shop/pages/member_page.dart'; //这个是苹果的风格
 
 class IndexPage extends StatefulWidget {
   @override
@@ -7,17 +11,47 @@ class IndexPage extends StatefulWidget {
 }
 
 class _IndexPageState extends State<IndexPage> {
-
   final List<BottomNavigationBarItem> bottomTabs = [
-    BottomNavigationBarItem(icon: Icon(CupertinoIcons.home),label: '首页'),
-    BottomNavigationBarItem(icon: Icon(CupertinoIcons.search),label: '分类'),
-    BottomNavigationBarItem(icon: Icon(CupertinoIcons.shopping_cart),label: '购物车'),
-    BottomNavigationBarItem(icon: Icon(CupertinoIcons.profile_circled),label: '会员中心'),
+    BottomNavigationBarItem(icon: Icon(CupertinoIcons.home), label: '首页'),
+    BottomNavigationBarItem(icon: Icon(CupertinoIcons.search), label: '分类'),
+    BottomNavigationBarItem(icon: Icon(CupertinoIcons.shopping_cart), label: '购物车'),
+    BottomNavigationBarItem(icon: Icon(CupertinoIcons.profile_circled), label: '会员中心'),
   ];
+
+  final List tabBodies = [
+    HomePage(),
+    CategoryPage(),
+    CartPage(),
+    MemberPage(),
+  ];
+
+  int currentIndex = 0;
+  var currentPage;
+
+  @override
+  void initState() {
+    super.initState();
+    currentPage = tabBodies[currentIndex];
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(244, 245, 245, 1.0),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: currentIndex,
+        items: bottomTabs,
+        onTap: (index) {
+          //这个是回调, 传入的是索引
+          setState(() {
+            currentIndex = index;//index赋值给currentindex , 刷新
+            currentPage = tabBodies[currentIndex];
+          });
+        },
+      ),
+      //这个逻辑就是上面的改变 , 下面的使用
+      body: currentPage,
+    );
   }
 }
-
