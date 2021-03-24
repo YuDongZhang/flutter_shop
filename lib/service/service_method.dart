64d8d5f8bcab199a111fc08042ff1fa2f0926578
YuDongZhang <977872239@qq.com>
2021-03-24 14:43:49 +0800

    09.首页_项目结构建立和获取数据(学会了如何进行url配置 , 请求接口的配置 , 还有页面的刷新

---
+++ lib/service/service_method.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+import 'package:dio/dio.dart';
+import 'package:flutter_shop/config/service_url.dart';
+
+//这个相当于我们的 retrofit 类 , 单独接口放到一起 , 还是和原始的 方法有点像
+Future getHomePageContent() async {
+  try {
+    print('开始获取首页数据...............');
+    Response response;
+    Dio dio = new Dio();
+    //表单的形式
+    dio.options.contentType = ContentType.parse("application/x-www-form-urlencoded") as String;
+    //传递参数 经纬度 , 模拟定位
+    var formData = {'lon': '115.02932', 'lat': '35.76189'};
+    //map类型 , homepage读取数据的方式 , 还有你传递的参数
+    response = await dio.post(servicePath['homePageContext'], data: formData);
+    if (response.statusCode == 200) {
+      return response.data;
+    } else {
+      throw Exception('后端接口出现异常，请检测代码和服务器情况.........');
+    }
+  } catch (e) {
+    return print('ERROR:======>${e}');
+  }
+}
