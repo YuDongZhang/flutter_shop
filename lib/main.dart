a89e1ffbf031ab61da462e30ad584021920c862f
YuDongZhang <977872239@qq.com>
2021-03-25 16:49:02 +0800

    24：Provide状态管理基础(不同的widget之间通讯)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_shop/pages/index_page.dart';
-
+import 'package:flutter_shop/provide/counter.dart';
+import 'package:provide/provide.dart';
 
 void main() {
-  runApp(MyApp());
+  var counter = Counter();
+  var providers = Providers();
+  //在顶层进行的依赖,全局可用
+  providers
+    //有多个可以再 .. , 注册依赖
+    ..provide(Provider<Counter>.value(counter));
+  runApp(ProviderNode(child: MyApp(), providers: providers));
 }
 
 class MyApp extends StatelessWidget {
@@ -13,9 +20,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: '百姓生活+',
         debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primaryColor:Colors.pink
-        ),
+        theme: ThemeData(primaryColor: Colors.pink),
         home: IndexPage(),
       ),
     );
