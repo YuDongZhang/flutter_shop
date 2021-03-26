ecc3019ee81e33b531a90e96f8b2d947782a83ca
YuDongZhang <977872239@qq.com>
2021-03-26 09:43:45 +0800

    32：列表页_小类高亮交互效果制作

--- lib/provide/child_category.dart
+++ lib/provide/child_category.dart
@@ -4,8 +4,11 @@ import 'package:flutter_shop/model/category.dart';
 
 class ChildCategory with ChangeNotifier {
   List<BxMallSubDto> childCategoryList = [];
+  int childIndex = 0;
 
+  ////点击大类时更换
   getChildCategory(List<BxMallSubDto> list) {
+    childIndex = 0; //默认恢复 0 , 对应的索引
     //解决页面没有横向列表中的全部的问题
     BxMallSubDto all = BxMallSubDto();
     all.mallSubId = '00';
@@ -15,5 +18,11 @@ class ChildCategory with ChangeNotifier {
     childCategoryList = [all];
     childCategoryList.addAll(list);
     notifyListeners(); //监听的通知
+  }
+
+  //改变子类索引
+  changeChildIndex(index) {//也是应该方法
+    childIndex = index;
+    notifyListeners();
   }
 }
