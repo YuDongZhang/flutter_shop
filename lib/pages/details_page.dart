69fd9e89f75c9af79517c6997d9b2f08cb66bff6
YuDongZhang <977872239@qq.com>
2021-03-27 12:42:00 +0800

    46：详细页_自建TabBar Widget

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_shop/provide/details_info.dart';
 import 'package:provide/provide.dart';
 
+import 'details_page/details_explain.dart';
 import 'details_page/details_page.dart';
+import 'details_page/details_tabbar.dart';
 
 class DetailsPage extends StatelessWidget {
   final String goodsId;
@@ -29,9 +31,11 @@ class DetailsPage extends StatelessWidget {
             builder: (context, snapshot) {
               if (snapshot.hasData) {
                 return Container(
-                    child: Column(
+                    child: ListView(
                   children: <Widget>[
                     DetailsTopArea(),
+                    DetailsExplain(),
+                    DetailsTabBar(),
                   ],
                 ));
               } else {
--- lib/provide/details_info.dart
+++ lib/provide/details_info.dart
@@ -6,9 +6,24 @@ import 'package:flutter_shop/service/service_method.dart';
 
 class DetailsInfoProvide with ChangeNotifier {
   DetailsModel goodsInfo = null;
+  //详情页的下面有一个 详情和评价
+  bool isLeft = true;
+  bool isRight = false;
+
+  //改变tabBar的状态
+  changeLeftAndRight(String changeState){
+    if(changeState=='left'){
+      isLeft=true;
+      isRight=false;
+    }else{
+      isLeft=false;
+      isRight=true;
+    }
+    notifyListeners();
+
+  }
 
   //从后台获取商品信息
-
   getGoodsInfo(String id) {
     var formData = {
       'goodId': id,
