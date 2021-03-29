ace38e245bf550cbdccf5b2f19253c8aa8d51135
YuDongZhang <977872239@qq.com>
2021-03-29 16:17:17 +0800

    50:持久化_shared_preferences基础1

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -2,44 +2,72 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_shop/provide/counter.dart';
 import 'package:provide/provide.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class CartPage extends StatelessWidget {
+class CartPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-      child: Column(
-        children: <Widget>[Number(), MyButton()],
-      ),
-    ));
-  }
+  _CartPageState createState() => _CartPageState();
 }
 
-class Number extends StatelessWidget {
+class _CartPageState extends State<CartPage> {
+  List<String> testList = [];
+
   @override
   Widget build(BuildContext context) {
+    _show(); //每次进入前进行显示
     return Container(
-        margin: EdgeInsets.only(top: 200),
-        child: Provide<Counter>(
-          builder: (context, child, counter) {
-            return Text(
-              '${counter.value}',
-              style: Theme.of(context).textTheme.display1,
-            );
+        child: Column(
+      children: <Widget>[
+        Container(
+          height: 500.0,
+          child: ListView.builder(
+            itemCount: testList.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(testList[index]),
+              );
+            },
+          ),
+        ),
+        RaisedButton(
+          onPressed: () {
+            _add();
           },
-        ));
-  }
-}
-
-class MyButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        child: RaisedButton(
-      onPressed: () {
-        Provide.value<Counter>(context).increment();
-      },
-      child: Text('递增'),
+          child: Text('增加'),
+        ),
+        RaisedButton(
+          onPressed: () {
+            _clear();
+          },
+          child: Text('清空'),
+        ),
+      ],
     ));
+  }
+
+  void _add() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String temp = "技术胖是最胖的!";
+    testList.add(temp);
+    prefs.setStringList('testInfo', testList);
+    _show();
+  }
+
+  void _show() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      if (prefs.getStringList('testInfo') != null) {
+        testList = prefs.getStringList('testInfo');
+      }
+    });
+  }
+
+  void _clear() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    //prefs.clear(); //全部清空
+    prefs.remove('testInfo'); //删除key键
+    setState(() {
+      testList = [];
+    });
   }
 }
